605e42a8c4a3f294fb8f1a66747dbabb48cf8bcc
Matthew Short <short.matthew.f@gmail.com>
2018-07-31 10:03:01 -0400

    hamburger + scrim

--- 01-junior/07-node-express/css_warmup/app.js
+++ 01-junior/07-node-express/css_warmup/app.js
@@ -1,6 +1,23 @@
 let trigger = document.querySelector('#modal-trigger')
 let modal = document.querySelector('#modal')
+let scrim = document.querySelector('.scrim')
+let main = document.querySelector('main')
 
 trigger.addEventListener('click', (evt) => {
-  modal.classList.toggle('active')
+  scrim.classList.add('active')
+  main.classList.add('blur')
+
+  setTimeout(() => {
+    modal.classList.add('active')
+  }, 30)
+})
+
+scrim.addEventListener('click', function (evt) {
+  if (evt.target === this) {
+    modal.classList.remove('active')
+    setTimeout(() => {
+      main.classList.remove('blur')
+      scrim.classList.remove('active')
+    }, 400)
+  }
 })
